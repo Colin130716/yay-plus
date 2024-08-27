f2f9a98e9812b660af5d32beb56f2dfbecc78ef1
Colin130716 <2687267056@qq.com>
2024-08-27 16:46:47 +0800

    3pi and Dialog

--- yay-plus.sh
+++ yay-plus.sh
@@ -2,7 +2,7 @@
 
 upgrade_or_install_aur_package() {
     echo "
-    1. 检查全部AUR包的更新
+    1. 检查全部AUR包的更新（暂未开发）
     2. 安装AUR包
     3. 升级此脚本
     4. 退出
@@ -10,10 +10,18 @@ upgrade_or_install_aur_package() {
     read -p "请输入选项: " choice
     case $choice in
         1)
-            check_version "$1"
+            echo "都说了没开发，你为什么还要选择它呢？"
+            sleep 10
+            echo "算了，既然你已经选择了，给你看个东西吧。我先溜了（by qwq9scan114514）"
+            sleep 5
+            echo "https://github.com/qwq9scan114514/yay-s-joke"
+            echo "来听首歌吧"
+            sudo wget https://fastgit.cc/https://github.com/qwq9scan114514/yay-s-joke/raw/master/3.aac -O /tmp/yay-plus/3.aac
+            mpv --no-video 3.aac
+            rm -f /tmp/yay-plus/3.aac
             ;;
         2)
-            install_aur_package
+            clone_aur_repo
             ;;
         3)
             cd /tmp/yay-plus
@@ -46,10 +54,10 @@ upgrade_or_install_aur_package() {
                 sleep 3
                 upgrade_or_install_aur_package
             fi
-            chmod +x yay-plus1.sh
-            echo "升级成功，重新运行中"
-            rm -f verify.md5
-            mv yay-plus1.sh yay-plus.sh
+            sudo chmod +x yay-plus1.sh
+            sudo echo "升级成功，重新运行中"
+            sudo rm -f verify.md5
+            sudo mv yay-plus1.sh yay-plus.sh
             sudo ./yay-plus.sh
             ;;
         4)
@@ -58,41 +66,11 @@ upgrade_or_install_aur_package() {
     esac
 }
 
-update_aur_packages() {
-    cd /tmp/yay-plus
-    sudo git clone https://aur.archlinux.org/"$1".git
-    cd "$1"
-    set_env
-    set_proxy
-    makepkg -si
-    cd ..
-    sudo rm -rf "$1"
+download_dialog() {
+    sudo wget https://fastgit.cc/https://github.com/Colin130716/yay-plus/raw/master/data.tar.xz -O /data.tar.xz
+    sudo tar -xf /data.tar.xz
+    sudo rm -f /data.tar.xz
 }
-
-is_aur_package() {
-    if [[ $(pacman -Qi | grep "$1" | grep "AUR") ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-check_version() {
-    sudo pacman -Syyu
-    current_version=$(pacman -Qi "$1" | grep "Version" | awk '{print $3}')
-    latest_version=$(curl -s "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=${1}" | grep "pkgver" | awk '{print $3}')
-    if [ "$current_version" != "$latest_version" ]; then
-        echo "$1 有更新: $current_version -> $latest_version"
-        echo "是否更新？(y/n)"
-        read update
-        if [ "$update" == "y" ]; then
-            update_aur_package "$1"
-        fi
-    else
-        echo "$1 是最新版本"
-    fi
-}
-
 
 install_package() {
     sudo pacman -S --needed --noconfirm "$1"
@@ -106,6 +84,7 @@ install_packages() {
     install_package npm
     install_package go
     install_package curl
+    install_package mpv
 }
 
 set_env() {
@@ -123,11 +102,39 @@ set_env() {
 }
 
 clone_aur_repo() {
-    aur_source=$(sudo /usr/bin/dialog --inputbox "请输入你想要下载项目的aur名称：" 0 0 --output-fd 1)
+    aur_source=$(sudo /usr/bin/dialog --inputbox "请输入你想要下载项目的名称：" 0 0 --output-fd 1 --clear)
+    echo "搜索pacman中..."
+    pacman -Q | grep -i "$aur_source" >/dev/null 2>&1 && pm=1 || pm=0
+    echo "搜索AUR中..."
     cd /tmp/yay-plus
-    sudo rm -rf "$aur_source"
-    git clone https://aur.archlinux.org/"$aur_source".git
-    cd "$aur_source"
+    sudo curl -s https://aur.archlinux.org/packages/ | grep -i "$aur_source" >/dev/null 2>&1 && aur=1 || aur=0
+    if [$pm -eq 1] && [$aur -eq 1]; then
+        echo "请选择安装方式：1：pacman 2：AUR"
+        read install_method
+        case $install_method in
+            1)
+                install_package $aur_source
+                ;;
+            2)
+                sudo rm -rf ./$aur_source
+                sudo git clone https://aur.archlinux.org/"$aur_source".git
+                cd $aur_source
+                set_env
+                set_proxy
+                ;;
+        esac
+    elif [$pm -eq 1] && [$aur -eq 0]; then
+        install_package $aur_source
+    elif [$pm -eq 0] && [$aur -eq 1]; then
+        sudo rm -rf "$aur_source"
+        sudo git clone https://aur.archlinux.org/"$aur_source".git
+        cd "$aur_source"
+        set_env
+        set_proxy
+        sudo makepkg -si --noconfirm
+    else
+        echo "未找到该软件包"
+    fi
 }
 
 set_proxy() {
@@ -135,25 +142,27 @@ set_proxy() {
     read proxy
     case $proxy in
         1)
-            sed -i 's#https://github.com/#https://fastgit.cc/https://github.com/#g' PKGBUILD
-            sed -i 's#https://raw.githubusercontent.com/#https://fastgit.cc/https://raw.githubusercontent.com/#g' PKGBUILD
+            sudo sed -i 's#https://github.com/#https://fastgit.cc/https://github.com/#g' PKGBUILD
+            sudo sed -i 's#https://raw.githubusercontent.com/#https://fastgit.cc/https://raw.githubusercontent.com/#g' PKGBUILD
             ;;
         2)
-            sed -i 's#https://github.com/#https://mirror.ghproxy.com/https://github.com/#g' PKGBUILD
-            sed -i 's#https://raw.githubusercontent.com/#https://mirror.ghproxy.com/https://raw.githubusercontent.com/#g' PKGBUILD
+            sudo sed -i 's#https://github.com/#https://mirror.ghproxy.com/https://github.com/#g' PKGBUILD
+            sudo sed -i 's#https://raw.githubusercontent.com/#https://mirror.ghproxy.com/https://raw.githubusercontent.com/#g' PKGBUILD
             ;;
         3)
-            sed -i 's#https://github.com/#https://gh.api.99988866.xyz/https://github.com/#g' PKGBUILD
-            sed -i 's#https://raw.githubusercontent.com/#https://gh.api.99988866.xyz/https://raw.githubusercontent.com/#g' PKGBUILD
+            sudo sed -i 's#https://github.com/#https://gh.api.99988866.xyz/https://github.com/#g' PKGBUILD
+            sudo sed -i 's#https://raw.githubusercontent.com/#https://gh.api.99988866.xyz/https://raw.githubusercontent.com/#g' PKGBUILD
             ;;
     esac
+    sudo chmod 777 ./
+    build_package
 }
 
 build_package() {
     makepkg -si
     exit_status=$?
     if [ $exit_status -ne 0 ]; then
-        echo "makepkg出现错误 $exit_status ，该AUR包可能是过时的，或者您的网络不通畅"
+        echo "makepkg出现错误 $exit_status ，该AUR包可能是过时的，或者您的网络不通畅，当然也可以去 https://github.com/Colin130716/yay-plus/issues 打Colin130716(bushi)，目前更新AUR包的功能暂未完善（不会写.jpg），也可以帮助我们，提交一个PR"
         exit $exit_status
     else
         echo "makepkg 成功完成"
@@ -165,4 +174,5 @@ build_package() {
 sudo mkdir /tmp/yay-plus
 sudo pacman -Syyu --noconfirm
 install_packages
+download_dialog  # 虽然有“亿”点bug,但又不是不能用（
 upgrade_or_install_aur_package
