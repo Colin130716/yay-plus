f217fdcd4a80c6cd8b57bdbe403cd3ba2833b402
qsdwindows <89772833+Colin130716@users.noreply.github.com>
2025-08-22 19:34:13 +0800

    增加配置文件

--- yay-plus.sh
+++ yay-plus.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # 全局变量
+readonly CONFIG_FILE="$HOME/.yay-plus/yay-plus.conf"
 readonly LOG_DIR="$HOME/.yay-plus/logs"
 readonly PACKAGE_DIR="$HOME/.yay-plus/packages"
 readonly CREATE_LOG_TIME=$(date +'%Y%m%d_%H%M%S')
@@ -15,11 +16,90 @@ readonly BLUE='\033[0;34m'
 readonly CYAN='\033[0;36m'
 readonly NC='\033[0m' # No Color
 
+# 默认配置
+DEFAULT_GITHUB_PROXY="1"
+DEFAULT_GO_PROXY="true"
+DEFAULT_NPM_PROXY="true"
+
 # 初始化函数
 init() {
     mkdir -p "$LOG_DIR" "$PACKAGE_DIR"
     now_time=$(date +'%Y/%m/%d %H:%M:%S')
     echo "[$now_time] 日志开始记录" >> "$LOG_DIR/$CREATE_LOG_TIME.log"
+    
+    # 检查并创建配置文件
+    check_and_create_config
+    
+    # 加载配置文件
+    load_config
+}
+
+# 检查并创建配置文件
+check_and_create_config() {
+    if [ ! -f "$CONFIG_FILE" ]; then
+        log "配置文件不存在，创建默认配置"
+        print_color "$YELLOW" "检测到配置文件不存在，正在创建默认配置文件: $CONFIG_FILE"
+        print_color "$YELLOW" "您可以在配置文件中设置默认的代理选项"
+        create_default_config
+        sleep 2
+    fi
+}
+
+# 加载配置文件
+load_config() {
+    if [ -f "$CONFIG_FILE" ]; then
+        log "加载配置文件: $CONFIG_FILE"
+        # 读取配置
+        DEFAULT_GITHUB_PROXY=$(get_config_value "github_proxy" "$DEFAULT_GITHUB_PROXY")
+        DEFAULT_GO_PROXY=$(get_config_value "go_proxy" "$DEFAULT_GO_PROXY")
+        DEFAULT_NPM_PROXY=$(get_config_value "npm_proxy" "$DEFAULT_NPM_PROXY")
+    else
+        log "配置文件不存在，使用默认配置"
+    fi
+}
+
+# 获取配置值
+get_config_value() {
+    local key="$1"
+    local default_value="$2"
+    local value
+    
+    value=$(grep -E "^$key=" "$CONFIG_FILE" 2>/dev/null | cut -d'=' -f2-)
+    if [ -z "$value" ]; then
+        echo "$default_value"
+    else
+        echo "$value"
+    fi
+}
+
+# 创建默认配置文件
+create_default_config() {
+    log "创建默认配置文件"
+    mkdir -p "$(dirname "$CONFIG_FILE")"
+    cat > "$CONFIG_FILE" << EOF
+# Yay+ 配置文件
+# 此文件用于设置 Yay+ 的默认行为
+
+# GitHub代理设置 (空:每次询问, 1-5:使用对应代理)
+# 1: https://github.akams.cn/ (推荐，采用Geo-IP 302重定向其他高速及镜像站)
+# 2: https://gh-proxy.com/ (推荐，下载速度快)
+# 3: https://ghfile.geekertao.top/ (推荐，速度快)
+# 4: https://gh.llkk.cc/ (速度较快)
+# 5: 不使用GitHub代理 (不推荐)
+github_proxy=$DEFAULT_GITHUB_PROXY
+
+# Go代理设置 (true:启用代理, false:不启用代理)
+# 启用后会使用 https://goproxy.cn 作为Go模块代理
+go_proxy=$DEFAULT_GO_PROXY
+
+# NPM代理设置 (true:启用代理, false:不启用代理)
+# 启用后会使用 https://registry.npmmirror.com 作为NPM镜像源
+npm_proxy=$DEFAULT_NPM_PROXY
+EOF
+    
+    print_color "$GREEN" "配置文件已创建: $CONFIG_FILE"
+    print_color "$CYAN" "默认设置: GitHub代理=1, Go代理=true, NPM代理=true"
+    print_color "$CYAN" "您可以编辑此文件来自定义默认行为"
 }
 
 # 日志记录函数
@@ -48,6 +128,225 @@ install_package() {
     log "安装软件包: $package"
     print_color "$BLUE" "安装软件包: $package"
     sudo pacman -S --needed --noconfirm "$package"
+}
+
+# 显示帮助信息
+show_help() {
+    cat << EOF
+Yay+ - AUR 助手增强版
+
+用法:
+  yay-plus [选项] [包名]
+
+选项:
+  -S, --install <包名>     安装软件包
+      --pacman             从官方仓库安装
+      --aur                从AUR安装
+      --flatpak            从Flatpak安装
+
+  -R, --remove <包名>      卸载软件包
+      --pacman             卸载官方仓库软件包
+      --flatpak            卸载Flatpak软件包
+
+  -Q, --query <包名>       查询软件包信息
+      --pacman             查询官方仓库软件包
+      --flatpak            查询Flatpak软件包
+
+  -U, --update             更新系统
+      --pacman             更新官方仓库软件包
+      --aur                更新AUR软件包
+      --flatpak            更新Flatpak软件包
+
+  -h, --help               显示此帮助信息
+  -v, --version            显示版本信息
+
+示例:
+  yay-plus -S --pacman firefox     从官方仓库安装Firefox
+  yay-plus -S --aur yay            从AUR安装yay
+  yay-plus -R --pacman firefox     卸载Firefox
+  yay-plus -Q --pacman firefox     查询Firefox信息
+  yay-plus -U --aur                更新所有AUR软件包
+EOF
+    exit 0
+}
+
+# 显示版本信息
+show_version() {
+    echo "Yay+ Version 3"
+    exit 0
+}
+
+# 解析命令行参数
+parse_args() {
+    local install_mode=""
+    local remove_mode=""
+    local query_mode=""
+    local update_mode=""
+    local package_name=""
+    local has_specific_mode=false
+    
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            -S|--install)
+                shift
+                package_name="$1"
+                shift
+                ;;
+            -R|--remove)
+                shift
+                package_name="$1"
+                remove_mode="true"
+                shift
+                ;;
+            -Q|--query)
+                shift
+                package_name="$1"
+                query_mode="true"
+                shift
+                ;;
+            -U|--update)
+                update_mode="true"
+                shift
+                ;;
+            --pacman|--aur|--flatpak)
+                has_specific_mode=true
+                if [ -n "$install_mode" ]; then
+                    install_mode="${1:2}"
+                elif [ -n "$remove_mode" ]; then
+                    remove_mode="${1:2}"
+                elif [ -n "$query_mode" ]; then
+                    query_mode="${1:2}"
+                elif [ -n "$update_mode" ]; then
+                    update_mode="${1:2}"
+                fi
+                shift
+                ;;
+            -h|--help)
+                show_help
+                ;;
+            -v|--version)
+                show_version
+                ;;
+            *)
+                # 如果没有指定模式，则认为是包名
+                if [ -z "$package_name" ]; then
+                    package_name="$1"
+                fi
+                shift
+                ;;
+        esac
+    done
+    
+    # 如果没有指定具体模式(--pacman/--aur/--flatpak)，则进入交互模式
+    if [ "$has_specific_mode" = false ]; then
+        log "未指定具体模式，进入交互模式"
+        return 1
+    fi
+    
+    # 执行相应操作
+    if [ -n "$install_mode" ] && [ -n "$package_name" ]; then
+        case $install_mode in
+            pacman) install_via_pacman "$package_name" ;;
+            aur) install_via_aur "$package_name" ;;
+            flatpak) install_via_flatpak "$package_name" ;;
+        esac
+        exit 0
+    elif [ -n "$remove_mode" ] && [ -n "$package_name" ]; then
+        case $remove_mode in
+            pacman) remove_via_pacman "$package_name" ;;
+            flatpak) remove_via_flatpak "$package_name" ;;
+        esac
+        exit 0
+    elif [ -n "$query_mode" ] && [ -n "$package_name" ]; then
+        case $query_mode in
+            pacman) query_via_pacman "$package_name" ;;
+            flatpak) query_via_flatpak "$package_name" ;;
+        esac
+        exit 0
+    elif [ -n "$update_mode" ]; then
+        case $update_mode in
+            pacman) update_pacman_packages ;;
+            aur) update_aur_packages ;;
+            flatpak) update_flatpak_packages ;;
+            *) update_system ;;
+        esac
+        exit 0
+    fi
+    
+    # 如果没有指定命令行参数，进入交互模式
+    return 1
+}
+
+# 通过pacman安装
+install_via_pacman() {
+    local package="$1"
+    log "命令行安装pacman包: $package"
+    sudo pacman -S --noconfirm "$package"
+}
+
+# 通过AUR安装
+install_via_aur() {
+    local package="$1"
+    log "命令行安装AUR包: $package"
+    
+    cd "$PACKAGE_DIR" || exit 1
+    sudo rm -rf "$package"
+    
+    if ! git clone "$AUR_BASE_URL/$package.git" >> "$LOG_DIR/$CREATE_LOG_TIME.log" 2>&1; then
+        log "git clone失败: $package"
+        print_color "$RED" "git clone失败，请检查网络连接或软件包名称"
+        exit 1
+    fi
+    
+    cd "$package" || exit 1
+    
+    if [ ! -f "PKGBUILD" ]; then
+        log "PKGBUILD不存在: $package"
+        print_color "$RED" "PKGBUILD不存在，可能不是有效的AUR包"
+        exit 1
+    fi
+    
+    # 处理依赖
+    process_dependencies
+    
+    set_env "noninteractive"
+    set_proxy "noninteractive"
+    makepkg -si --skippgpcheck --noconfirm
+}
+
+# 通过flatpak安装
+install_via_flatpak() {
+    local package="$1"
+    log "命令行安装flatpak包: $package"
+    flatpak install -y flathub "$package"
+}
+
+# 通过pacman卸载
+remove_via_pacman() {
+    local package="$1"
+    log "命令行卸载pacman包: $package"
+    sudo pacman -Rsn --noconfirm "$package"
+}
+
+# 通过flatpak卸载
+remove_via_flatpak() {
+    local package="$1"
+    log "命令行卸载flatpak包: $package"
+    flatpak uninstall -y "$package"
+}
+
+# 通过pacman查询
+query_via_pacman() {
+    local package="$1"
+    log "命令行查询pacman包: $package"
+    pacman -Si "$package" || pacman -Qi "$package"
+}
+
+# 通过flatpak查询
+query_via_flatpak() {
+    local package="$1"
+    log "命令行查询flatpak包: $package"
+    flatpak info "$package" || flatpak list | grep "$package"
 }
 
 # 主菜单函数
@@ -305,31 +604,49 @@ run_flatpak_package() {
 
 # 设置环境变量
 set_env() {
+    local mode="${1:-interactive}"
+    
     # Go代理设置
-    read -rp "需要使用go代理下载吗？（代理下载地址：https://goproxy.cn）(y/N): " set_go_proxy
-    if [[ "$set_go_proxy" =~ ^[Yy]$ ]]; then
-        log "设置Go代理: https://goproxy.cn"
-        export GO111MODULE=on
-        export GOPROXY=https://goproxy.cn
+    if [ "$mode" = "noninteractive" ]; then
+        if [ "$DEFAULT_GO_PROXY" = "true" ]; then
+            log "设置Go代理: https://goproxy.cn"
+            export GO111MODULE=on
+            export GOPROXY=https://goproxy.cn
+        fi
     else
-        read -rp "是否还原为默认代理下载地址？(Y/n): " set_go_proxy_default
-        if [[ ! "$set_go_proxy_default" =~ ^[Nn]$ ]]; then
-            log "还原Go代理: https://proxy.golang.org"
-            export GOPROXY=https://proxy.golang.org
+        read -rp "需要使用go代理下载吗？（代理下载地址：https://goproxy.cn）(y/N): " set_go_proxy
+        if [[ "$set_go_proxy" =~ ^[Yy]$ ]]; then
+            log "设置Go代理: https://goproxy.cn"
+            export GO111MODULE=on
+            export GOPROXY=https://goproxy.cn
+        else
+            read -rp "是否还原为默认代理下载地址？(Y/n): " set_go_proxy_default
+            if [[ ! "$set_go_proxy_default" =~ ^[Nn]$ ]]; then
+                log "还原Go代理: https://proxy.golang.org"
+                export GOPROXY=https://proxy.golang.org
+            fi
         fi
     fi
     
     # NPM代理设置
-    read -rp "需要使用npm代理吗？（代理地址：https://registry.npmmirror.com）(y/N): " set_npm_proxy
-    if [[ "$set_npm_proxy" =~ ^[Yy]$ ]]; then
-        log "设置NPM代理: https://registry.npmmirror.com"
-        npm config set registry https://registry.npmmirror.com
-        sudo npm config set registry https://registry.npmmirror.com
+    if [ "$mode" = "noninteractive" ]; then
+        if [ "$DEFAULT_NPM_PROXY" = "true" ]; then
+            log "设置NPM代理: https://registry.npmmirror.com"
+            npm config set registry https://registry.npmmirror.com
+            sudo npm config set registry https://registry.npmmirror.com
+        fi
     else
-        read -rp "是否还原为默认代理下载地址？(Y/n): " set_npm_proxy_default
-        if [[ ! "$set_npm_proxy_default" =~ ^[Nn]$ ]]; then
-            log "还原NPM代理: https://registry.npmjs.org"
+        read -rp "需要使用npm代理吗？（代理地址：https://registry.npmmirror.com）(y/N): " set_npm_proxy
+        if [[ "$set_npm_proxy" =~ ^[Yy]$ ]]; then
+            log "设置NPM代理: https://registry.npmmirror.com"
+            npm config set registry https://registry.npmmirror.com
+            sudo npm config set registry https://registry.npmmirror.com
+        else
+            read -rp "是否还原为默认代理下载地址？(Y/n): " set_npm_proxy_default
+            if [[ ! "$set_npm_proxy_default" =~ ^[Nn]$ ]]; then
+                log "还原NPM代理: https://registry.npmjs.org"
             npm config set registry https://registry.npmjs.org
+            fi
         fi
     fi
     
@@ -351,10 +668,40 @@ set_env() {
 
 # 设置代理
 set_proxy() {
+    local mode="${1:-interactive}"
+    
+    if [ "$mode" = "noninteractive" ] && [ -n "$DEFAULT_GITHUB_PROXY" ]; then
+        # 非交互模式，使用配置的代理
+        case $DEFAULT_GITHUB_PROXY in
+            1)
+                log "使用GitHub代理: https://github.akams.cn/"
+                sed -i 's#https://github.com/#https://github.akams.cn/https://github.com/#g' PKGBUILD
+                sed -i 's#https://raw.githubusercontent.com/#https://github.akams.cn/https://raw.githubusercontent.com/#g' PKGBUILD
+                ;;
+            2)
+                log "使用GitHub代理: https://gh-proxy.com/"
+                sed -i 's#https://github.com/#https://gh-proxy.com/https://github.com/#g' PKGBUILD
+                sed -i 's#https://raw.githubusercontent.com/#https://gh-proxy.com/https://raw.githubusercontent.com/#g' PKGBUILD
+                ;;
+            3)
+                log "使用GitHub代理: https://ghfile.geekertao.top/"
+                sed -i 's#https://github.com/#https://ghfile.geekertao.top/https://github.com/#g' PKGBUILD
+                sed -i 's#https://raw.githubusercontent.com/#https://ghfile.geekertao.top/https://raw.githubusercontent.com/#g' PKGBUILD
+                ;;
+            4)
+                log "使用GitHub代理: https://gh.llkk.cc/"
+                sed -i 's#https://github.com/#https://gh.llkk.cc/https://github.com/#g' PKGBUILD
+                sed -i 's#https://raw.githubusercontent.com/#https://gh.llkk.cc/https://raw.githubusercontent.com/#g' PKGBUILD
+                ;;
+        esac
+        return
+    fi
+    
+    # 交互模式
     echo "请选择GitHub代理："
-    echo "1. https://fastgit.cc/（目前测试速度较慢）"
+    echo "1. https://github.akams.cn/（推荐，采用Geo-IP 302重定向其他高速及镜像站）"
     echo "2. https://gh-proxy.com/（推荐，下载速度快）"
-    echo "3. https://gh.api.99988866.xyz/（备用,不稳定）"
+    echo "3. https://ghfile.geekertao.top/（推荐，速度快）"
     echo "4. https://gh.llkk.cc/（速度较快）"
     echo "5. 不使用GitHub代理（不推荐）"
     
@@ -362,9 +709,9 @@ set_proxy() {
     
     case $proxy in
         1)
-            log "使用GitHub代理: https://fastgit.cc/"
-            sed -i 's#https://github.com/#https://fastgit.cc/https://github.com/#g' PKGBUILD
-            sed -i 's#https://raw.githubusercontent.com/#https://fastgit.cc/https://raw.githubusercontent.com/#g' PKGBUILD
+            log "使用GitHub代理: https://github.akams.cn/"
+            sed -i 's#https://github.com/#https://github.akams.cn/https://github.com/#g' PKGBUILD
+            sed -i 's#https://raw.githubusercontent.com/#https://github.akams.cn/https://raw.githubusercontent.com/#g' PKGBUILD
             ;;
         2)
             log "使用GitHub代理: https://gh-proxy.com/"
@@ -372,9 +719,9 @@ set_proxy() {
             sed -i 's#https://raw.githubusercontent.com/#https://gh-proxy.com/https://raw.githubusercontent.com/#g' PKGBUILD
             ;;
         3)
-            log "使用GitHub代理: https://gh.api.99988866.xyz/"
-            sed -i 's#https://github.com/#https://gh.api.99988866.xyz/https://github.com/#g' PKGBUILD
-            sed -i 's#https://raw.githubusercontent.com/#https://gh.api.99988866.xyz/https://raw.githubusercontent.com/#g' PKGBUILD
+            log "使用GitHub代理: https://ghfile.geekertao.top/"
+            sed -i 's#https://github.com/#https://ghfile.geekertao.top/https://github.com/#g' PKGBUILD
+            sed -i 's#https://raw.githubusercontent.com/#https://ghfile.geekertao.top/https://raw.githubusercontent.com/#g' PKGBUILD
             ;;
         4)
             log "使用GitHub代理: https://gh.llkk.cc/"
@@ -427,8 +774,8 @@ process_dependencies() {
                 process_dependencies
                 
                 # 构建并安装依赖
-                set_env
-                set_proxy
+                set_env "noninteractive"
+                set_proxy "noninteractive"
                 makepkg -si --skippgpcheck --noconfirm
                 
                 # 返回原目录
@@ -574,6 +921,14 @@ system_check() {
 main() {
     init
     system_check
+    
+    # 尝试解析命令行参数
+    if parse_args "$@"; then
+        # 如果解析成功并执行了命令，直接退出
+        exit 0
+    fi
+    
+    # 否则进入交互模式
     install_required_packages
     
     clear
